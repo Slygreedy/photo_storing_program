74838235550307de9e1eca36a39ff2a1a461d470
bg64 <13July1974>
2017-03-01 11:11:26 +0000

    output de-duped photos to file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import javax.xml.bind.DatatypeConverter;
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,24 +9,22 @@ import java.util.Map;
 public class Main {
 
     public static void main(String argv[]) {
-        File file = new File("d:/pic");
+        File file = new File("d:/oldpic");
         Map<String, File> hashcodeToFileCrossRef = new HashMap<String, File>();
         for (File photo : file.listFiles()) {
-            savePhotoWithoutDuplication(hashcodeToFileCrossRef, photo, generateHashCodeFromPhotoData(photo));
+            recordPhotoWithoutDuplication(hashcodeToFileCrossRef, photo, generateHashCodeFromPhotoData(photo));
+        }
+        for (Map.Entry<String, File> element : hashcodeToFileCrossRef.entrySet()) {
+            copyPicToNewLocation(element);
         }
     }
 
-    private static void savePhotoWithoutDuplication(Map<String, File> fileMap, File photo, String hex) {
+    private static void recordPhotoWithoutDuplication(Map<String, File> fileMap, File photo, String hex) {
         if (fileMap.containsKey(hex)) {
             System.out.println("duplicate");
             return;
         }
         insertIntoHashcodeToFileCrossRef(fileMap, photo, hex);
-    }
-
-    private static void insertIntoHashcodeToFileCrossRef(Map<String, File> fileMap, File photo, String hex) {
-        fileMap.put(hex, photo);
-        System.out.printf("hex=%s\n", hex);
     }
 
     public static String generateHashCodeFromPhotoData(File file) {
@@ -39,6 +38,24 @@ public class Main {
             ex.printStackTrace();
             return null;
         }
+    }
+
+    private static void copyPicToNewLocation(Map.Entry<String, File> element) {
+        System.out.println("key: " + element.getKey() + " filename: " + element.getValue().getName());
+        try {
+            File newFile = new File("d:/newpic/" + element.getValue().getName());
+            Files.copy(element.getValue().toPath(),
+                    newFile.toPath(),
+                    StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException("copy failed for file:" + element.getValue().getName());
+        }
+    }
+
+    private static void insertIntoHashcodeToFileCrossRef(Map<String, File> fileMap, File photo, String hex) {
+        fileMap.put(hex, photo);
+        System.out.printf("hex=%s\n", hex);
     }
 
     /* *previous code that created an array first but above does not require it*
