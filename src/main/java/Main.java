a2482439d06ec204516835aaa2bbe925b937b177
bg64 <Simon.Read@uk.tesco.com>
2017-03-28 08:58:16 +0100

    update traverse method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,46 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.security.MessageDigest;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Main {
 
     public static void main(String argv[]) {
-        File file = new File("d:/oldpic");
+
+
+        //moved instantiating this object and converting it to hashmap out of traverse method
+        File file = new File("/Users/simonread/pictures/oldpic");
         Map<String, File> hashcodeToFileCrossRef = new HashMap<String, File>();
-        for (File photo : file.listFiles()) {
-            recordPhotoWithoutDuplication(hashcodeToFileCrossRef, photo, generateHashCodeFromPhotoData(photo));
-        }
-        for (Map.Entry<String, File> element : hashcodeToFileCrossRef.entrySet()) {
-            copyPicToNewLocation(element);
-        }
+
+        //look at implementing more try/catch and throws
+
+        //all of these methods are static because 1.main is and 2.do not require object to be instantiated
+
     }
+
+    private static void traverse(File file,Map<String,File> collection) {
+
+        //what about duplicates (I think I have covered types below)?
+        Iterator it = FileUtils.iterateFiles(new File("/Users/simonread/pictures/oldpic"), ".jpg", false);
+            while (it.hasNext()){
+                if (file.isDirectory())
+                {
+                    traverse(File file,Map<String,File> collection);
+                }
+                else {
+                    for (File photo : file.listFiles())
+                        {
+                        recordPhotoWithoutDuplication(collection, photo, generateHashCodeFromPhotoData(photo));
+                        }
+                    for (Map.Entry<String, File> element : collection.entrySet())
+                        {
+                        copyPicToNewLocation(element);
+                        }
+                }
+            }
+        }
+
 
     private static void recordPhotoWithoutDuplication(Map<String, File> fileMap, File photo, String hex) {
         if (fileMap.containsKey(hex)) {
@@ -43,7 +69,7 @@ public class Main {
     private static void copyPicToNewLocation(Map.Entry<String, File> element) {
         System.out.println("key: " + element.getKey() + " filename: " + element.getValue().getName());
         try {
-            File newFile = new File("d:/newpic/" + element.getValue().getName());
+            File newFile = new File("/Users/simonread/pictures/newpic/" + element.getValue().getName());
             Files.copy(element.getValue().toPath(),
                     newFile.toPath(),
                     StandardCopyOption.REPLACE_EXISTING);
@@ -57,6 +83,8 @@ public class Main {
         fileMap.put(hex, photo);
         System.out.printf("hex=%s\n", hex);
     }
+
+    //below is for persistence of directory conetnts as an array
 
     /* *previous code that created an array first but above does not require it*
     String dirPath = "d:/pic";
